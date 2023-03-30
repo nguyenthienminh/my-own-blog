6e673f46ba5ff64b29d8dd8b77ebea379bca7475
nguyenthienminh <86407326+nguyenthienminh@users.noreply.github.com>
2023-03-30 08:03:53 +0700

    Updated date

--- Website/date.js
+++ Website/date.js
@@ -1,3 +1,3 @@
 window.onload = function(){
-    document.getElementById('date').innerHTML = "This page was last updated at 9:40am on the 27th of March, 2023.";
+    document.getElementById('date').innerHTML = "This page was last updated at 8am on the 30th of March, 2023.";
 };
